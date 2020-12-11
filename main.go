61c0454dd1203028649296974231800f498dc9c5
Andrew Kraynak <abk89@miami.edu>
2020-12-11 13:17:47 -0500

    Added length input gathering

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import {
+import(
 	"fmt"
-}
+)
 
 func main() {
-	
+	fmt.Printf("Random Password Generator\n")
+
+	fmt.Printf("Enter the minimum length : ")
+	var minLen int
+	fmt.Scanf("%d", &minLen)
+
+	fmt.Printf("Enter the maximum length : ")
+	var maxLen int
+	fmt.Scanf("%d", &maxLen)
+
+	fmt.Printf("You entered minLen=%v and maxLen=%v \n", minLen, maxLen)
 }
